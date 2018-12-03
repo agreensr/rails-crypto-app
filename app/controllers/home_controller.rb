67d3d0d271441cc0c670e1b7812947b262f23c1c
Antoine Green <antoinegreensr@gmail.com>
2018-12-03 04:27:21 +0000

    Adding coincapapi v2 functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,48 @@
 class HomeController < ApplicationController
+  
   def index
-    @hello = "Hey from the controller"
+   require 'net/http'
+   require 'json'
+   
+   @url = 'https://api.coinmarketcap.com/v2/ticker/'
+   @uri = URI(@url)
+   @response = Net::HTTP.get(@uri)
+   @coins = JSON.parse(@response)
+   @coins = @coins["data"]
+   
+   @my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM"]
+   
   end
+  
+  def lookup
+    
+   require 'net/http'
+   require 'json'
+   
+   @url = 'https://api.coinmarketcap.com/v2/ticker/'
+   @uri = URI(@url)
+   @response = Net::HTTP.get(@uri)
+   @lookup_coins = JSON.parse(@response)
+   @lookup_coins = @lookup_coins["data"]
+    
+    # Get the sym from the input field
+    @symbol = params[:sym]
+    if @symbol 
+      @symbol = @symbol.upcase
+    end
+    
+    # if @symbol == ""
+    #   @symbol = "You need to enter a Crypto Symbol"
+    # end
+    
+    # @error = "That symbol does not exsist"
+    
+  end
+  
     
   def about
     @about = "My name is Antoine Green"
   end
+  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "home/about"
+  get "home/lookup"
+  # Lookup form route
+  post "/home/lookup" => "home/lookup"
   root "home#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
